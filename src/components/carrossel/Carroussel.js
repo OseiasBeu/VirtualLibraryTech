e4b4db2dd0ae8b6132df0610ad468e4eba69e9cc
Oseias Beu <oseiasbeu@outlook.com>
2018-05-24 09:38:44 -0300

    Imagens do banner sendo passadas como parâmetros

--- src/components/carrossel/Carroussel.js
+++ src/components/carrossel/Carroussel.js
@@ -1,20 +1,20 @@
 import React, {Component} from 'react';
 import './Carroussel.css';
 import Carousell from 'nuka-carousel';
-import com1 from './../img/banner/evento1.jpg';
-import com2 from './../img/banner/evento2.jpg';
-import com3 from './../img/banner/evento3.jpg';
-import com4 from './../img/banner/evento4.jpg';
+// import com1 from './../img/banner/evento1.jpg';
+// import com2 from './../img/banner/evento2.jpg';
+// import com3 from './../img/banner/evento3.jpg';
+// import com4 from './../img/banner/evento4.jpg';
 
 class Carroussel extends Component {
-  render() {
+  render() {    
       return (
           <div className="carousel externo">
               <Carousell slideWidth={"800px"}>
-                  <img className="carousel" src={com1} />
-                  <img className="carousel" src={com2}/>
-                  <img className="carousel" src={com3} />
-                  <img className="carousel" src={com4} /> 
+                  <img className="carousel" src={this.props.imagem1} />
+                  <img className="carousel" src={this.props.imagem2}/>
+                  <img className="carousel" src={this.props.imagem3} />
+                  <img className="carousel" src={this.props.imagem4} /> 
               </Carousell>                
           </div>
       );
--- src/segundaPagina/FirstBook.js
+++ src/segundaPagina/FirstBook.js
@@ -1,5 +1,9 @@
 import React,{Component}  from 'react';
-import Carroussel from '../components/carrossel/Carroussel'
+import Carroussel from '../components/carrossel/Carroussel';
+import com1 from './../components/img/banner/evento1.jpg';
+import com2 from './../components/img/banner/evento2.jpg';
+import com3 from './../components/img/banner/evento3.jpg';
+import com4 from './../components/img/banner/evento4.jpg';
 
 import './FirstBook.css';
 
@@ -8,7 +12,7 @@ class FirstBook extends Component{
     render(){
         return(
             <div>
-            <Carroussel/>    
+            <Carroussel imagem1={com1} imagem2={com2} imagem3={com3} imagem4={com4}/>    
                 <p>Funciona....</p>
                 </div>
           
